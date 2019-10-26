4b89cb108dada650f4fd2eb1f3c64bcfadc868ba
Reynaldo Aguilar <reynaldo@canva.com>
2019-10-26 13:03:08 +1100

    Initial Commit

---
+++ MasterMoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterMoviesApp
+//
+//  Created by Reynaldo Aguilar on 26/10/19.
+//  Copyright © 2019 Reynaldo Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
