d7019845b19af824e0747bced44dad5074193d1d
Reynaldo Aguilar <reynaldo@canva.com>
2019-11-26 19:57:48 +1100

    Add empty space

--- MasterMoviesApp/MoviesDiscoverer.swift
+++ MasterMoviesApp/MoviesDiscoverer.swift
@@ -21,6 +21,7 @@ public struct MoviesDiscoverer {
         let policy = ReleasedMoviesPolicy(date: date)
         let theaterInterval = policy.theaterInterval
         var theaterResult: Result<[Movie], Error>!
+        
         dispatchGroup.enter()
         client.request(endpoint: TMDBApi.Discover.releasedMovies(from: theaterInterval.from, to: theaterInterval.to)) { result in
             theaterResult = result
