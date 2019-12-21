608180cec4825cd01eebcfaf4b1f42ce07d055da
Reynaldo Aguilar <reynaldo@canva.com>
2019-12-21 22:42:40 +1100

    Remove color theme dependency from font theme

--- MasterMoviesApp/FontTheme.swift
+++ MasterMoviesApp/FontTheme.swift
@@ -8,69 +8,18 @@
 
 import UIKit
 
+public struct TextStyle {
+    public let font: UIFont
+    public let kern: Float
+    public let lineSpacing: CGFloat
+    
+    public static let `default` = TextStyle(font: UIFont.systemFont(ofSize: 12), kern: 1, lineSpacing: 0)
+}
+
 public struct FontTheme {
-    public let colorTheme: ColorTheme // TODO: Refactor this class so that it doesn't need the reference to the color theme
-    public func largeTitle(string: String) -> NSAttributedString {
-        return attributtedString(
-            string: string,
-            foregroundColor: colorTheme.secondaryTextColor,
-            font: UIFont.systemFont(ofSize: 30),
-            kern: 1.2,
-            lineSpacing: 28
-        )
-    }
-    
-    public func title(string: String) -> NSAttributedString {
-        return attributtedString(
-            string: string,
-            foregroundColor: colorTheme.primaryTextColor,
-            font: UIFont.systemFont(ofSize: 20),
-            kern: 1.2,
-            lineSpacing: 28
-        )
-    }
-    
-    public func subtitle(string: String) -> NSAttributedString {
-        return attributtedString(
-            string: string,
-            foregroundColor: colorTheme.primaryTextColor,
-            font: UIFont.systemFont(ofSize: 16),
-            kern: 1.2,
-            lineSpacing: 28
-        )
-    }
-    
-    public func body(string: String) -> NSAttributedString {
-        return attributtedString(
-            string: string,
-            foregroundColor: colorTheme.secondaryTextColor,
-            font: UIFont.systemFont(ofSize: 14),
-            kern: 1,
-            lineSpacing: 0
-        )
-    }
-    
-    public func small(string: String) -> NSAttributedString {
-        return attributtedString(
-            string: string,
-            foregroundColor: colorTheme.secondaryTextColor,
-            font: UIFont.systemFont(ofSize: 10),
-            kern: 1,
-            lineSpacing: 0
-        )
-    }
-    
-    private func attributtedString(string: String, foregroundColor: UIColor, font: UIFont, kern: Float, lineSpacing: CGFloat = 0) -> NSAttributedString {
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = lineSpacing
-        
-        return NSAttributedString(
-            string: string,
-            attributes: [
-                .foregroundColor: foregroundColor,
-                .font: font,
-                .kern: kern
-            ]
-        )
-    }
+    public let largeTitle: TextStyle = TextStyle(font: UIFont.systemFont(ofSize: 30), kern: 1.2, lineSpacing: 28)
+    public let title: TextStyle = TextStyle(font: UIFont.systemFont(ofSize: 20), kern: 1.2, lineSpacing: 28)
+    public let subtitle: TextStyle = TextStyle(font: UIFont.systemFont(ofSize: 16), kern: 1.2, lineSpacing: 28)
+    public let body: TextStyle = TextStyle(font: UIFont.systemFont(ofSize: 14), kern: 1, lineSpacing: 0)
+    public let small: TextStyle = TextStyle(font: UIFont.systemFont(ofSize: 10), kern: 1, lineSpacing: 0)
 }
